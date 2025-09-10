1917635c56f5aff9ca4182d3fba99038eeed343b
SomnathVN <nalawadesomnath2003@gmail.com>
2025-09-10 23:55:19 +0530

    added logging (with logback.xml)

--- src/main/java/com/example/jounralrestapi/config/SpringSecurityConfiguration.java
+++ src/main/java/com/example/jounralrestapi/config/SpringSecurityConfiguration.java
@@ -1,5 +1,8 @@
 package com.example.jounralrestapi.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -17,7 +20,12 @@ import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@Slf4j
+//Logging without making instance
 public class SpringSecurityConfiguration {
+
+    //Logging by making instance
+    //private static final Logger logger = LoggerFactory.getLogger(SpringSecurityConfiguration.class);
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -41,10 +49,16 @@ public class SpringSecurityConfiguration {
         return new BCryptPasswordEncoder(){
             @Override
             public boolean matches(CharSequence rawPassword, String encodedPassword) {
-                System.out.println("Raw: " + rawPassword);
-                System.out.println("Encoded: " + encodedPassword);
+                //System.out.println("Raw: " + rawPassword);
+                //logger.info("Raw: " + rawPassword);
+                log.info("Raw: " + rawPassword);
+                //System.out.println("Encoded: " + encodedPassword);
+                //logger.info("Encoded: " + encodedPassword);
+                log.info("Encoded: " + encodedPassword);
                 boolean result = super.matches(rawPassword, encodedPassword);
-                System.out.println("Match result = " + result);
+                //System.out.println("Match result = " + result);
+                //logger.warn("Match result = " + result);
+                log.warn("Match result = " + result);
                 return result;
             }
         };
